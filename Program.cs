8edfb5105d28b5ede277655a59208244c8fd9a2a
dvir <dvirarazi0@gmail.com>
2024-11-21 13:57:56 +0200

    commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿class Program
+{
+  //sizes
+  //bedrooms
+  //floors
+  //years
+  static double[][] x = default!;
+  //prices
+  static double[] y = default!;
+  static int m = default!;
+  static int n = default!;
+
+  static void Main()
+  {
+    var lines = File
+      .ReadAllLines("houses.txt")
+      .Select(line => line
+        .Split(',')
+        .Select(item => double.Parse(item))
+        .ToArray()
+      )
+      .ToArray();
+
+    x = (double[][])lines.Select(line => line[..^1]);
+    y = (double[])lines.Select(line => line[^1]);
+
+    m = x.Length;
+    n = y.Length;
+  }
+}
